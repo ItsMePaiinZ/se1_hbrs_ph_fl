82b3f5b7a57fa859400b07a7a0895b289aaf6898
Philipp Hagedorn <philipp.hagedorn@smail.inf.h-brs.de>
2022-10-18 21:00:59 +0200

    start

--- src/uebungen/uebung2/Container.java
+++ src/uebungen/uebung2/Container.java
@@ -4,39 +4,26 @@ import java.util.ArrayList;
 
 public class Container {
 
-private ArrayList<Member> members = new ArrayList<>();
+    ArrayList<Member> members = new ArrayList<>();
 
-public void addMember(Member member) throws ContainerException {
-    if(members.stream().filter(m -> m.getID() == member.getID()).count() > 0)
-        throw new ContainerException(member.getID());
-    members.add(member);
-}
-
-public String deleteMember(Integer id){
-    if(members.stream().filter(m -> m.getID() == id).count() == 0)
-        return "Es gibt keinen Member mit der ID " + id + " in dem Container!";
-    members.remove(members.stream().filter(m -> m.getID() == id).findAny().get());
-    return "moin!";
-}
-
-public void dump(){
-    System.out.println(members);
-}
-
-public int size(){
-    return members.size();
-}
-
-    public static void main(String[] args) throws ContainerException {
-        Container c = new Container();
-        ConcreteMember cm = new ConcreteMember();
-
-        for(int i = 0; i < 10; i++)
-            c.addMember(new ConcreteMember());
-        c.addMember(cm);
-        c.dump();
-        c.deleteMember(0);
-        c.dump();
+    public void addMember(Member member) throws ContainerException {
+        if (members.stream().filter(m -> m.getID() == member.getID()).count() > 0)
+            throw new ContainerException(member.getID());
+        members.add(member);
     }
 
+    public String deleteMember(Integer id) {
+        if (members.stream().filter(m -> m.getID() == id).count() == 0)
+            return "Es gibt keinen Member mit der ID " + id + " in dem Container!";
+        members.remove(members.stream().filter(m -> m.getID() == id).findAny().get());
+        return "Member mit der ID " + id + " wurde entfernt!";
+    }
+
+    public void dump() {
+        System.out.println(members);
+    }
+
+    public int size() {
+        return members.size();
+    }
 }
